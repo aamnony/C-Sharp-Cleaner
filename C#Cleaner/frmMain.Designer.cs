694fc3b9532bb747e240b47ff373c9e06e4dec04
amnonya <aamnony@gmail.com>
2015-10-08 19:53:05 +0300

    Add code

---
+++ C#Cleaner/frmMain.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace CSharp_Cleaner
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlActions = new System.Windows.Forms.Panel();
+            this.btnUninstall = new System.Windows.Forms.Button();
+            this.btnRefreshList = new System.Windows.Forms.Button();
+            this.lstApps = new System.Windows.Forms.ListBox();
+            this.pnlActions.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlActions
+            // 
+            this.pnlActions.Controls.Add(this.btnUninstall);
+            this.pnlActions.Controls.Add(this.btnRefreshList);
+            this.pnlActions.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlActions.Location = new System.Drawing.Point(1, 5);
+            this.pnlActions.Name = "pnlActions";
+            this.pnlActions.Size = new System.Drawing.Size(346, 35);
+            this.pnlActions.TabIndex = 1;
+            // 
+            // btnUninstall
+            // 
+            this.btnUninstall.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnUninstall.Enabled = false;
+            this.btnUninstall.Location = new System.Drawing.Point(112, 0);
+            this.btnUninstall.Margin = new System.Windows.Forms.Padding(4);
+            this.btnUninstall.Name = "btnUninstall";
+            this.btnUninstall.Size = new System.Drawing.Size(234, 35);
+            this.btnUninstall.TabIndex = 3;
+            this.btnUninstall.Text = "Uninstall selection";
+            this.btnUninstall.UseVisualStyleBackColor = true;
+            this.btnUninstall.Click += new System.EventHandler(this.btnUninstall_Click);
+            // 
+            // btnRefreshList
+            // 
+            this.btnRefreshList.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnRefreshList.Location = new System.Drawing.Point(0, 0);
+            this.btnRefreshList.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRefreshList.Name = "btnRefreshList";
+            this.btnRefreshList.Size = new System.Drawing.Size(112, 35);
+            this.btnRefreshList.TabIndex = 2;
+            this.btnRefreshList.Text = "Refresh";
+            this.btnRefreshList.UseVisualStyleBackColor = true;
+            this.btnRefreshList.Click += new System.EventHandler(this.btnRefreshList_Click);
+            // 
+            // lstApps
+            // 
+            this.lstApps.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstApps.FormattingEnabled = true;
+            this.lstApps.ItemHeight = 18;
+            this.lstApps.Location = new System.Drawing.Point(1, 40);
+            this.lstApps.Name = "lstApps";
+            this.lstApps.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.lstApps.Size = new System.Drawing.Size(346, 408);
+            this.lstApps.TabIndex = 0;
+            this.lstApps.SelectedIndexChanged += new System.EventHandler(this.lstApps_SelectedIndexChanged);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 448);
+            this.Controls.Add(this.lstApps);
+            this.Controls.Add(this.pnlActions);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmMain";
+            this.Padding = new System.Windows.Forms.Padding(1, 5, 1, 0);
+            this.ShowIcon = false;
+            this.Text = "C#Cleaner";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.pnlActions.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlActions;
+        private System.Windows.Forms.Button btnUninstall;
+        private System.Windows.Forms.Button btnRefreshList;
+        private System.Windows.Forms.ListBox lstApps;
+
+    }
+}
+
---
+++ C#Cleaner/frmMain.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CSharp_Cleaner
+{
+    public partial class frmMain : Form
+    {
+        #region Fields
+
+        private static readonly string LOCAL_APP_DATA_PATH = Environment.SpecialFolder.LocalApplicationData.GetPath();
+
+        #endregion
+
+        #region Constructors
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+
+        #region Events
+
+        private void btnRefreshList_Click(object sender, EventArgs e)
+        {
+            FillAppsListBox();
+        }
+
+        private void btnUninstall_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show(this,
+                String.Format("The following directories will be permanently deleted:\n{0}", lstApps.SelectedItems.ToLines()),
+                "Warning",
+                MessageBoxButtons.OKCancel) == System.Windows.Forms.DialogResult.OK)
+            {
+                try
+                {
+                    foreach (var item in lstApps.SelectedItems)
+                    {
+                        Directory.Delete(GetFullPath(item), true);
+                    }
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(this, ex.Message, "Error");
+                }
+                finally
+                {
+                    FillAppsListBox();
+                }
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            FillAppsListBox();
+        }
+
+        private void lstApps_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnUninstall.Enabled = (lstApps.SelectedIndices.Count > 0);
+        }
+
+        #endregion
+
+        #region Methods
+
+        private static string GetFullPath(object path)
+        {
+            return String.Format("{0}\\{1}", LOCAL_APP_DATA_PATH, path);
+        }
+
+        private void FillAppsListBox()
+        {
+            lstApps.Items.Clear();
+            lstApps.Items.AddRange(Directory.EnumerateDirectories(LOCAL_APP_DATA_PATH).Select(Path.GetFileName).ToArray());
+        }
+
+        #endregion
+    }
+
+    static class Extensions
+    {
+        #region Methods
+
+        internal static string GetPath(this Environment.SpecialFolder specialFolder)
+        {
+            return Environment.GetFolderPath(specialFolder);
+        }
+
+        internal static string ToLines(this ListBox.SelectedObjectCollection objs)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (string s in objs)
+            {
+                sb.AppendLine(s);
+            }
+            return sb.ToString();
+        }
+
+        #endregion
+    }
+}
